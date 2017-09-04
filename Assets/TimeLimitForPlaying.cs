1e8e2d172b628587ebe546fb0e6d3f4efecf94ff
zenmaibane <koteicream.1024@gmail.com>
2017-09-05 00:00:59 +0900

    アイコンの追加と起動期限の追加

---
+++ Assets/TimeLimitForPlaying.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeLimitForPlaying : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    var datetimeStr = System.DateTime.Now;
+	    var timeLimit = new DateTime(2017, 9, 14, 00, 00, 00);
+        if (datetimeStr > timeLimit)
+	    {
+	        Application.Quit();
+	    }
+    }
+}
